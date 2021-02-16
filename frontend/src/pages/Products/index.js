2e7ec766e74e4e7f0083abee18b0bb18e3599d98
Bluenarysun <tee.emilond@gmail.com>
2021-02-17 01:49:16 +0700

    Pagination already

--- frontend/src/pages/Products/index.js
+++ frontend/src/pages/Products/index.js
@@ -1,5 +1,4 @@
-import React, { useEffect } from 'react';
-// import _ from 'lodash';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getRobots } from 'apis';
 import { setRobots } from 'redux/robot';
@@ -7,20 +6,34 @@ import { setRobots } from 'redux/robot';
 /* Component */
 import RobotImage from './RobotImage';
 
+/* Lib */
+import ReactPaginate from 'react-paginate';
+
 const Products = () => {
   const dispatch = useDispatch();
   const robots = useSelector((state) => state.robot.robots);
 
+  const [offset, setOffset] = useState(0);
+  const [perPage] = useState(20);
+  const [pageCount, setPageCount] = useState(0);
+
   const initRobots = async () => {
     const res = await getRobots();
+    const robotsSlice = res.data.data.slice(offset, offset + perPage);
     if (res && res.data) {
-      dispatch(setRobots(res.data.data));
+      dispatch(setRobots(robotsSlice));
+      setPageCount(Math.ceil(res.data.data.length / perPage));
     }
   };
 
   useEffect(() => {
     initRobots();
-  }, []);
+  }, [offset]);
+
+  const handlePageChange = (e) => {
+    const selectedPage = e.selected;
+    setOffset(selectedPage + 1);
+  };
 
   return (
     <div className="container mx-auto mt-10">
@@ -32,6 +45,20 @@ const Products = () => {
           <RobotImage key={robot.rid} robot={robot} />
         ))}
       </div>
+
+      <ReactPaginate
+        previousLabel={'prev'}
+        nextLabel={'next'}
+        breakLabel={'...'}
+        breakClassName={'break-me'}
+        pageCount={pageCount}
+        marginPagesDisplayed={2}
+        pageRangeDisplayed={5}
+        onPageChange={handlePageChange}
+        containerClassName={'pagination'}
+        subContainerClassName={'pages pagination'}
+        activeClassName={'active'}
+      />
     </div>
   );
 };
