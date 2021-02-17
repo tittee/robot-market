f15789f94f2fbea4c8dec3552dc4d1d62fe231b2
Bluenarysun <tee.emilond@gmail.com>
2021-02-17 23:39:16 +0700

    Remove old code.

--- frontend/src/hooks/useCart.js
+++ frontend/src/hooks/useCart.js
@@ -3,17 +3,9 @@ import { useDispatch } from 'react-redux';
 import { setActiveCart, setCarts } from 'redux/cart';
 import { setStock, setOutOfStock } from 'redux/robot';
 
-let localCart = localStorage.getItem('cart');
-
 const useCart = ({ initItem, carts}) => {
   const dispatch = useDispatch();
-  useEffect(() => {
-    //turn it into js
-    localCart = JSON.parse(localCart);
-    //load persisted cart into state if it exists
-    if (localCart) setCarts(localCart);
-  }, []); //the empty array ensures useEffect only runs once
-
+ 
   const addToCart = (item) => {
     if (!item) {
       return;
